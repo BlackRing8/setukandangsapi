2ba9d3bb1bc556d027c703e21b368e1b976daf96
BlackRing8 <gprakoso123@gmail.com>
2024-08-08 18:59:52 +0700

    update meta data

--- components/home-component/ProjectCard.jsx
+++ components/home-component/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({ project }) => {
     <Card className="mt-4 overflow-y-hidden">
       <CardHeader>
         <div className="relative w-full h-[250px] flex items-center">
-          <Image className="absolute bottom-0 shadow-2xl rounded-xl h-64" src={project.image} width={247} height={250} alt="" priority />
+          <Image className="absolute bottom-0 shadow-2xl rounded-xl h-64" src={project.image} width={247} height={250} alt="Fasilitas situ kandang sapi" priority />
         </div>
       </CardHeader>
       <div className="h-full px-8 py-2">
